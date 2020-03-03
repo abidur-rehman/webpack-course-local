07b1559802062ed8f5fa639cdea93e5b62b347f9
Abid Ur-Rehman <abid.ur-rehman1@dwp.gov.uk>
2020-03-03 20:50:36 +0000

    first commit

---
+++ src/main.js
@@ -0,0 +1,11 @@
+require("babel-runtime/regenerator")
+require("./main.css")
+require("./index.html")
+
+var a = async (args) => {
+    const {a, b} = args;
+    await console.log("Hello from arrow function", a, b);
+    console.log("done");
+}
+
+a({a:1, b:2})
