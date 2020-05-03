6d2b9d0864979ba698f7a1b44c4dadfdcf8ff094
Abid Ur-Rehman <abid.ur-rehman1@dwp.gov.uk>
2020-05-03 15:44:14 +0100

    Added dynamic importing and universal component.

--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -1,9 +1,15 @@
 import React from 'react';
 
+const getBundle = () => {
+    import("lodash").then(_ => {
+        console.log("imported", _)
+    })
+}
+
 const Gallery = () => {
     return (
         <div>
-            <h1>Gallery</h1>
+            <h1 onClick={getBundle}>Gallery</h1>
         </div>
     );
 }
